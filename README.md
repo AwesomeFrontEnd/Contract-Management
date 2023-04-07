8f45205c54a940e90767219ff456e09ac124f8a9
Romance <faaa@live.com>
2023-04-07 14:20:07 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# Contract-Management
-合同管理中台，使用Acro Design构建。
+# Arco Design Pro
+
+in **Vite** + React
+
+## 快速开始
+
+```
+// 初始化项目
+npm install
+
+// 开发模式
+npm run dev
+
+// 构建
+npm run build
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Layout } from '@arco-design/web-react';
+import { FooterProps } from '@arco-design/web-react/es/Layout/interface';
+import cs from 'classnames';
+import styles from './style/index.module.less';
+
+function Footer(props: FooterProps = {}) {
+  const { className, ...restProps } = props;
+  return (
+    <Layout.Footer className={cs(styles.footer, className)} {...restProps}>
+      使用 Arco Design Pro 构建
+    </Layout.Footer>
+  );
+}
+
+export default Footer;
